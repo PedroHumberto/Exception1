20286dbf497c5999488b18632c9ce0bde89e2c20
Pedro Humberto <pedrohumbertogc@gmail.com>
2021-07-08 22:22:24 -0300

    Exception1

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Exception1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try{
+                int n1 = int.Parse(Console.ReadLine());
+                int n2 = int.Parse(Console.ReadLine());
+
+                int result = n1 / n2;
+
+                Console.WriteLine(result);                
+            }
+            catch(DivideByZeroException e){ //use to catch de exception and give a better message
+                Console.WriteLine("Error! " + e.Message);
+            }
+        }
+    }
+}
